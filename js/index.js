9e296f15672d568a971bec6b764bbb786a8399d4
Ellie <eleanorholt97@gmail.com>
2024-05-06 16:39:27 +0100

    Add axios cdn link to HTML and implement AI API within JS file

--- js/index.js
+++ js/index.js
@@ -1,10 +1,24 @@
-function generatePoem(event) {
-  event.preventDefault();
+function displayPoem(response) {
+  console.log("poem generated");
   new Typewriter("#poem", {
-    strings: `Joy and woe are woven fine, <br />A clothing for the soul divine`,
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let userInput = document.querySelector("#user-input").value;
+  let prompt = `Generate a poem about ${userInput}`;
+  let context = "You are a poet";
+  let apiKey = "839e3cb4ta48e140e587fa20cb9532o6";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("generating poem");
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let formElement = document.querySelector("#prompt-form");
