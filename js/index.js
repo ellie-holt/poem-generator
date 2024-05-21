f2950431a91dfe070c006d5f3f791ffb3bc267a0
Ellie <eleanorholt97@gmail.com>
2024-05-21 20:58:25 +0100

    Add welcome message and tweak CSS sizes and margins

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 let generatedPoem = null;
 let poemElement = document.querySelector("#poem");
 let formElement = document.querySelector("#prompt-form");
-let generatingMessage = document.querySelector("#generating-message");
+let generatingMessage = document.querySelector(".generating-message");
+let welcomeMessage = document.querySelector(".welcome-message");
 
 generatedPoem = new Typewriter(poemElement, {
   delay: 25,
@@ -21,10 +22,11 @@ function generatePoem(event) {
   let userInput = document.querySelector("#user-input").value;
   let prompt = `Generate a poem about ${userInput}`;
   let context =
-    "You are a poet capable of writing poems in a variety of styles. Please write a 4-6 line poem considering the user's prompt. Write the poem using this format: `<h2>Poem Title</h2><hr /><p>poem content</p> Use a <br /> tag after each line which has a line coming after it, but DO NOT include a line break after the final line. The poem should not end with a <br /> tag.";
+    "You are a poet capable of writing poems in a variety of styles. Please write a 4-8 line poem considering the user's prompt. Write the poem using this format: `<h2>Poem Title</h2><hr /><p>line of poem</p>. Use a seperate <p> element for each line. Only write one poem per user prompt.";
   let apiKey = "839e3cb4ta48e140e587fa20cb9532o6";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  welcomeMessage.classList.add("disappear");
   poemElement.classList.remove("hidden");
   generatingMessage.classList.remove("hidden");
   generatingMessage.innerHTML = `Generating a poem about ${userInput}...`;
